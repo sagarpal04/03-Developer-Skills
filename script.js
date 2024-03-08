a950088da522ef89bd36ebe218fbe16950d9e677
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 17:02:03 +0530

    Coding Challenge #1

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
-let num = -2;
-let i = 1;
-while (i <= 10) {
-  debugger;
-  num = Math.min(num, -1);
-  i += 1;
-}
-console.log(num);
+const printForcast = function (temp) {
+  let ans = '';
+  for (let i = 0; i < temp.length; i += 1) {
+    ans += `... ${temp[i]}°C in ${i + 1} days `;
+  }
+  ans += '...';
+  console.log(ans);
+};
+printForcast([17, 21, 23]);
+printForcast([12, 5, -5, 0, 4]);
