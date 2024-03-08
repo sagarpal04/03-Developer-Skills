3502d039650139eeefb22220177905b037c9109b
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 16:47:17 +0530

    Debugger

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const calcAmplitude = function (temp1, temp2) {
-  let mini = Infinity;
-  let maxi = -Infinity;
-  const temperature = temp1.concat(temp2);
-  for (let i = 0; i < temperature.length; i += 1) {
-    if (typeof temperature[i] !== 'number') continue;
-    mini = Math.min(mini, temperature[i]);
-    maxi = Math.max(maxi, temperature[i]);
-  }
-  return maxi - mini;
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('Degree celsius : ')),
+  };
+  // console.log(measurement);
+  console.table(measurement);
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
 };
-
-const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-console.log('Amplitude : ', calcAmplitude(temperature, [100, -100]));
+console.log(measureKelvin());
