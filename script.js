0d2253fe11ccaa669f021e3dd2e39c05140eaff9
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 16:26:27 +0530

    Using Google, StackOverflow  and MDN

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const calcAmplitude = function (temp1, temp2) {
+  let mini = Infinity;
+  let maxi = -Infinity;
+  const temperature = temp1.concat(temp2);
+  for (let i = 0; i < temperature.length; i += 1) {
+    if (typeof temperature[i] !== 'number') continue;
+    mini = Math.min(mini, temperature[i]);
+    maxi = Math.max(maxi, temperature[i]);
+  }
+  return maxi - mini;
+};
+
+const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+console.log('Amplitude : ', calcAmplitude(temperature, [100, -100]));
